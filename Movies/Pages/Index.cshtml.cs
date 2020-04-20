7d47ecf7c0110bbcb326ec7673b19c434102e864
Brandon Bednar <bwbednar@ksu.edu>
2020-04-20 14:09:34 -0500

    continued work with second lecutre. Left off at Applying MPAA Rating Filters

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -10,9 +10,34 @@ namespace Movies.Pages
     public class IndexModel : PageModel
     {
 
+        /// <summary>
+        /// The movies to display on the index page 
+        /// </summary>
+        public IEnumerable<Movie> Movies { get; protected set; }
+
+        /// <summary>
+        /// The filtered MPAA Ratings
+        /// </summary>
+        public string[] MPAARatings { get; set; }
+
+
+        /// <summary>
+        /// The current search terms 
+        /// </summary>
+        public string SearchTerms { get; set; }
+
+        /// <summary>
+        /// Gets the search results for display on the page
+        /// </summary>
         public void OnGet()
         {
+            SearchTerms = Request.Query["SearchTerms"];
+            Movies = MovieDatabase.Search(SearchTerms);
+            MPAARatings = Request.Query["MPAARatings"];
+
 
         }
+
+
     }
 }
